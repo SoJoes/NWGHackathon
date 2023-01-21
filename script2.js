af90db4d5a3143c10c9ace369c95e648fdc18a65
SoJoes <sphiej223@gmail.com>
2023-01-21 16:35:24 +0000

    file structure

--- script2.js
+++ script2.js
@@ -1,13 +1,33 @@
-function reddenPage(){
-    document.body.style.backgroundColor = "red";
-    console.log("HI");
+function ssh() {
+  function onload() {
+    let animating = false;
+    let eating = false;
+    const size = {
+      height: 120,
+      width: 120,
+    };
+    const position = {
+      x: 0,
+      y: 0,
+    };
+    const pet = document.createElement('div');
+    pet.id = "mypet";
+    updatePosition(100, 100);
+    pet.classList.add('ssh-pet');
+    document.body.appendChild(pet);
+
+    function updatePosition(x, y) {
+      pet.style.left = (position.x = x) + 'px';
+      pet.style.top = (position.y = y) + 'px';
+    }
+  }
+  window.addEventListener('load', onload);
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    if(!tab.url.includes("chrome://")){
-        chrome.scripting.executeScript({
-            target:{ tabId: tab.id},
-            function: reddenPage
-        });
-    }
-})
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.text = `(${ssh.toString()})()`;
+script.onload = function() {
+  this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(script);
